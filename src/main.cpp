e9c9697de87cf1d74c4d29a53966f721002a88dd
PyDev19 <atharvamishra956+1@gmail.com>
2023-07-29 23:27:20 -0500

    added code to show qml gui

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <QQmlApplicationEngine>
+#include <QGuiApplication>
+#include <QIcon>
+#include <QObject>
+#include <QUrl>
+#include <QQmlContext>
+#include "include/backend.hpp"
 
-int num = 3;
+int main (int argc, char *argv[]) {
+	QGuiApplication app(argc, argv);
+	
+	QQmlApplicationEngine engine;
+	QQmlContext *root_context = engine.rootContext();
 
-int main() {
-    std::cout << "Hello, World" << std::endl;
-	return 0;
+	Backend backend;
+	root_context->setContextProperty("backend", &backend);
+
+	engine.load(QUrl("qrc:qml/main.qml"));
+
+	QList<QObject*> root_objects = engine.rootObjects();
+	if (root_objects.isEmpty())
+		return -1;
+
+	// QObject *application_window = engine.rootObjects().value(0);
+	// QObject::connect(&backend, SIGNAL(output_ready(QVariant)), application_window, SLOT(get_command_output(QVariant)));
+
+	return app.exec();
 }
